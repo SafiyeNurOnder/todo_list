1dcf2c1a5a18cd351542b2572df84c95f02eb799
SafiyeNurOnder <safiyenuronder@gmail.com>
2024-04-01 14:44:14 +0300

    .

---
+++ login.py
@@ -0,0 +1,63 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtWidgets import QDialog, QMessageBox
+from PyQt5.uic import loadUi
+
+
+from models import session
+from models.user import User
+
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi("login.ui", self)
+        self.pushButton_login.clicked.connect(self.loginfunction)
+        self.lineEdit_password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.pushButton_signup.clicked.connect(self.gotosignup)
+
+        # email alanı için doğrulayıcı ekleme
+        email_validator= QRegExpValidator(QRegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"))
+        self.lineEdit_email.setValidator(email_validator)
+
+        # şifre için doğrulayıcı ekleme
+        password_validator = QRegExpValidator(QRegExp(".{8,}"))
+        self.lineEdit_password.setValidator(password_validator)
+
+    def loginfunction(self):
+        email = self.lineEdit_email.text()
+        password = self.lineEdit_password.text()
+
+        # email alanının doğruluğunu kontrol etme
+        if not self.lineEdit_email.hasAcceptableInput():
+            QMessageBox.warning(self, "Warning", "Invalid email format!")
+            return
+
+        # şifre alanının doğruluğunu kontrol etme
+        if not self.lineEdit_password.hasAcceptableInput():
+            QMessageBox.warning(self, "Warning", "Invalid password format! Password must be at least 8 characters long.")
+            return
+
+        # kullanıcıyı veritabanında sorgulama - READ
+        user = session.query(User).filter_by(email=email).first()
+        if user and user.password == password:
+            print("Successfully logged in with email: ", email, "and password: ", password)
+            self.gotosidebar()
+        else:
+            QMessageBox.warning(self, "Warning", "Login Failed!")
+
+
+    def gotosignup(self):
+        from signup import SignUp
+        signup = SignUp()
+        from app import widget
+        widget.addWidget(signup)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def gotosidebar(self):
+        from sidebar import SideBar
+        sidebar = SideBar()
+        from app import widget
+        widget.addWidget(sidebar)
+        widget.setCurrentIndex(widget.currentIndex()+1)
---
+++ signup.py
@@ -0,0 +1,64 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtWidgets import QDialog, QMessageBox
+from PyQt5.uic import loadUi
+
+from models import session
+from models.user import User
+
+class SignUp(QDialog):
+    def __init__(self):
+        super(SignUp, self).__init__()
+        loadUi("signup.ui", self)
+        self.pushButton_signupui.clicked.connect(self.signupfunction)
+        self.lineEdit_password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_confirmpassword.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.pushButton_login.clicked.connect(self.gotologin)
+
+        # email alanı için doğrulayıcı ekleme
+        email_validator = QRegExpValidator(QRegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"))
+        self.lineEdit_email.setValidator(email_validator)
+
+        # şifre için doğrulayıcı ekleme
+        password_validator = QRegExpValidator(QRegExp(".{8,}"))
+        self.lineEdit_password.setValidator(password_validator)
+
+    def signupfunction(self):
+        username = self.lineEdit_username.text()
+        email = self.lineEdit_email.text()
+        if self.lineEdit_password.text() == self.lineEdit_confirmpassword.text():
+            password = self.lineEdit_password.text()
+
+            # Alanların doluluğunu kontrol etme
+            if not (username and email and password):
+                QMessageBox.warning(self, "Warning", "Please fill all fields!")
+                return
+
+            # email alanının doğruluğunu kontrol etme
+            if not self.lineEdit_email.hasAcceptableInput():
+                QMessageBox.warning(self, "Warning", "Invalid email format!")
+                return
+
+            # şifre alanının doğruluğunu kontrol etme
+            if not self.lineEdit_password.hasAcceptableInput():
+                QMessageBox.warning(self, "Warning", "Invalid password format! Password must be at least 8 characters long.")
+                return
+
+            # yeni kullanıcıyı veritabanına ekle - CREATE
+            new_user = User(username=username, email=email, password=password)
+            session.add(new_user)
+            session.commit()
+
+            from app import widget
+            QMessageBox.information(self, "Success", "Successfully signed up!")
+            login=widget()
+            widget.addWidget(login)
+            widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def gotologin(self):
+        from login import Login
+        from app import widget
+        login=Login()
+        widget.addWidget(login)
+        widget.setCurrentIndex(widget.currentIndex()+1)
