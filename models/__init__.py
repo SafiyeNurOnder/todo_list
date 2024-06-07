6dfd0c35d1b4ff6b223679ce771034f5cc9013bd
SafiyeNurOnder <safiyenuronder@gmail.com>
2024-06-07 09:56:42 +0300

    .

--- models/__init__.py
+++ models/__init__.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#import os
+import os
 
-#ip=os.environ["IP"]
+ip=os.environ["IP"]
 
 # MySQL veritabanı bağlantısı oluşturma
-#engine = create_engine('mysql+pymysql://root:1@'+ip+':3306/todoList')
-engine = create_engine('mysql+pymysql://root:1@localhost:3306/todo_list')
+engine = create_engine('mysql+pymysql://root:1@'+ip+':3306/todoList')
+#engine = create_engine('mysql+pymysql://root:1@localhost:3306/todo_list')
 Base = declarative_base()
 
 # Oturum oluşturma
